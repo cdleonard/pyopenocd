7d10e1e77f015a4c1e3e77d647cb9357858a034a
Leonard Crestez <leonard.crestez@nxp.com>
2020-02-07 19:53:43 +0200

    setup.py: Initial setup.py
    Signed-off-by: Leonard Crestez <leonard.crestez@nxp.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python3
+
+from setuptools import setup
+
+setup(
+    name='pyopenocd',
+    version='0.1',
+    author='Leonard Crestez',
+    author_email='cdleonard@gmail.com',
+    url='https://github.com/cdleonard/pyopenocd',
+    packages=['openocd'],
+)
