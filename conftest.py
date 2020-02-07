715a9ab11f93df3543491db42eda5f344cc8506d
Leonard Crestez <leonard.crestez@nxp.com>
2020-02-07 22:54:46 +0200

    Add need_openocd_running marker
    Signed-off-by: Leonard Crestez <leonard.crestez@nxp.com>

--- conftest.py
+++ conftest.py
@@ -8,5 +8,6 @@ def pytest_collection_modifyitems(config, items):
     if not config.getoption('--openocd-running'):
         skip_marker = pytest.mark.skip(reason="needs --openocd-running")
         for item in items:
-            if "tclrpc" in item.fixturenames:
+            if ("tclrpc" in item.fixturenames or
+                    list(item.iter_markers(name='need_openocd_running'))):
                 item.add_marker(skip_marker)
