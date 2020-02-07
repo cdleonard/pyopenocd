15acffee09527e5eb51ce0818f8aa5c42fdebc56
Leonard Crestez <leonard.crestez@nxp.com>
2020-02-07 20:29:32 +0200

    test_tclrpc: Test expr 1234
    Signed-off-by: Leonard Crestez <leonard.crestez@nxp.com>

--- openocd/test_tclrpc.py
+++ openocd/test_tclrpc.py
@@ -1,8 +1,11 @@
 import pytest
 from . import OpenOcdTclRpc
 
-def test_tclrpc_expr(tclrpc):
+def test_tclrpc_expr1(tclrpc):
     assert(tclrpc.sendrecv('expr 1 + 1') == '2')
+
+def test_tclrpc_expr2(tclrpc):
+    assert(tclrpc.sendrecv('expr 1234') == '1234')
 
 @pytest.fixture(scope='function')
 def tclrpc():
